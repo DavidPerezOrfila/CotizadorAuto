64b58fc9003ac0b419d292a08dd0ea45b6ab9b7f
David Pérez <knightbcn@gmail.com>
2019-05-01 15:58:11 +0200

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Formulario from './Formulario';
+import {obtenerDiferenciaAnio} from '../helper';
+
 
 class App extends Component {
 	cotizarSeguro = (datos) => {
-    console.log(datos)
+    // console.log(datos)
+    const {marca, plan, year} = datos;
+    // Agregar una base de 2000
+    let resultado = 2000;
+
+    // Obtener la diferencia de años
+    const diferencia = obtenerDiferenciaAnio(year);
+    // por cada año restar el 3%
+    resultado -= ((diferencia *3) * resultado) / 100;
+    console.log(resultado);
+    // Americano 15%, Asiático 5% y Europeo 30% de incremento al valor actual
+
   };
 	render() {
 		return (
