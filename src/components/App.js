0945de68f356bd1cea3fe6a3e8383318eb6e128b
David Pérez <knightbcn@gmail.com>
2019-05-02 10:05:58 +0200

    add 2019 year

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Formulario from './Formulario';
-import {obtenerDiferenciaAnio} from '../helper';
+import {obtenerDiferenciaAnio, calcularMarca, obtenerPlan} from '../helper';
 
 
 class App extends Component {
 	cotizarSeguro = (datos) => {
-    // console.log(datos)
     const {marca, plan, year} = datos;
     // Agregar una base de 2000
     let resultado = 2000;
@@ -14,10 +13,14 @@ class App extends Component {
     // Obtener la diferencia de años
     const diferencia = obtenerDiferenciaAnio(year);
     // por cada año restar el 3%
-    resultado -= ((diferencia *3) * resultado) / 100;
-    console.log(resultado);
+    resultado -= ((diferencia * 3) * resultado) / 100;
     // Americano 15%, Asiático 5% y Europeo 30% de incremento al valor actual
-
+    resultado = calcularMarca(marca) * resultado;
+    // El plan del auto; el básico aumenta el valor 20% y la cobertura completa 50%
+    let incrementoPlan = obtenerPlan(plan);
+    // Dependiendo del plan incrementar
+    resultado = parseFloat(incrementoPlan * resultado).toFixed(2);
+    console.log(resultado);
   };
 	render() {
 		return (
